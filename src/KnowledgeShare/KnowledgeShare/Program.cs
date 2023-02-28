536c4c88be0932d4257e5eaa2454e9de52f1f2d5
Callum White <callumwhi@live.co.uk>
2023-02-28 23:09:31 +0000

    add neo4j

--- src/KnowledgeShare/KnowledgeShare/Program.cs
+++ src/KnowledgeShare/KnowledgeShare/Program.cs
@@ -25,12 +25,21 @@ builder.Services.AddRazorPages()
 builder.Services.AddScoped<IDriver>(
         (serviceProvider) =>
         {
+#if DEBUG
             IConfiguration configuration = serviceProvider.GetService<IConfiguration>();
             IConfigurationSection neoConfig = configuration.GetSection("Neo4j");
             IConfigurationSection neoConfigUri = neoConfig.GetSection("uri");
             IConfigurationSection neoConfigUser = neoConfig.GetSection("user");
             IConfigurationSection neoConfigPassword = neoConfig.GetSection("password");
             return GraphDatabase.Driver(neoConfigUri.Value, AuthTokens.Basic(neoConfigUser.Value, neoConfigPassword.Value));
+#endif
+
+#if !DEBUG
+            string boltUrl = Environment.GetEnvironmentVariable("NEO4J_URI");
+            string username = Environment.GetEnvironmentVariable("NEO4J_USERNAME");
+            string password = Environment.GetEnvironmentVariable("NEO4J_PASSWORD");
+            return GraphDatabase.Driver(boltUrl, AuthTokens.Basic(username, password));
+#endif
         }
     );
 
