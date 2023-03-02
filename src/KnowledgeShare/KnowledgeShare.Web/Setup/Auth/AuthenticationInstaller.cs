cacebf845bc09aa3f70de1d414cc9a15f48ef214
Callum White <callumwhi@live.co.uk>
2023-03-02 22:44:11 +0000

    fix ref

--- src/KnowledgeShare/KnowledgeShare.Web/Setup/Auth/AuthenticationInstaller.cs
+++ src/KnowledgeShare/KnowledgeShare.Web/Setup/Auth/AuthenticationInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 
 namespace KnowledgeShare.Web.Setup.Auth;
