946a5032bb962e0a66eea63a23a39fa201330b9a
a-whitec <callum.white@mhr.co.uk>
2023-03-06 10:26:57 +0000

    remove middleware check

--- src/KnowledgeShare/KnowledgeShare.Web/Setup/Middleware/CheckUserMiddleware.cs
+++ src/KnowledgeShare/KnowledgeShare.Web/Setup/Middleware/CheckUserMiddleware.cs
@@ -19,15 +19,6 @@ public class CheckUserMiddleware
         var identity = context.User.Identity;
         if (identity != null && identity.IsAuthenticated)
         {
-            var personService = context.RequestServices.GetService<IPersonService>();
-            var graphClient = context.RequestServices.GetService<GraphServiceClient>();
-            User user = await graphClient!.Me.Request().GetAsync();
-            Person? person = await personService!.GetPersonByUserIdAsync(user.Id);
-
-            if (person == null)
-            {
-                await personService!.CreatePersonAsync(new CreatePersonDto(user.Id, user.DisplayName));
-            } 
         }
         await _next(context);
     }
