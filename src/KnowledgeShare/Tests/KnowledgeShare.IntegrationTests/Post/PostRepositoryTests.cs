f71d8eb0278d171a62bc03a72354e680add8ebb1
Callum White <callumwhi@live.co.uk>
2023-06-06 00:30:09 +0100

    report

---
+++ src/KnowledgeShare/Tests/KnowledgeShare.IntegrationTests/Post/PostRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using KnowledgeShare.Core.People;
+using KnowledgeShare.Core.Posts.Types;
+using KnowledgeShare.Core.Tags;
+using KnowledgeShare.Persistence.People;
+using KnowledgeShare.Persistence.Posts;
+using KnowledgeShare.Persistence.Social;
+
+namespace KnowledgeShare.IntegrationTests.Post;
+
+public class PostRepositoryTests : IntegrationTestsDatabase
+{
+    [Test]
+    public async Task GivenPost_WhenGetGetByIdAsync_ReturnPost()
+    {
+        await WithSessionAsync(async session =>
+        {
+            PersonRepository personRepository = new PersonRepository(session);
+            Person person = Person.Create("1", "Callum", String.Empty);
+            await personRepository.AddAsync(person);
+            ArticlePostRepository articlePostRepository = new ArticlePostRepository(session);
+            ArticlePost articlePost = ArticlePost.Create(person, "Test Article", "https://example.com/article",
+                "This is a test article.");
+            articlePost.Tags = new List<Tag>();
+            await articlePostRepository.CreateAsync(articlePost);
+            ArticlePost? articlePost2 = await articlePostRepository.GetByIdAsync(articlePost.Id);
+            Assert.IsNotNull(articlePost2);
+            Assert.AreEqual(articlePost2.Id, articlePost.Id);
+        });
+    }
+}
